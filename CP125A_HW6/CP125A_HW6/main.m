1a64e2983924706d32dad530dbbb41d28827c66e
Jared <jared@jsorge.net>
2014-02-26 07:14:36 -0800

    Created Xcode project.

---
+++ CP125A_HW6/CP125A_HW6/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CP125A_HW6
+//
+//  Created by Jared Sorge on 2/26/14.
+//  Copyright (c) 2014 jsorge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JMSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JMSAppDelegate class]));
+    }
+}
