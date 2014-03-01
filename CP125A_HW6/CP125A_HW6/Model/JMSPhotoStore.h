3515b10e76764037264652ac014b29cac197b1a8
Jared <jared@jsorge.net>
2014-03-01 11:19:26 -0800

    Implemented saving an loading, updated documentation

--- CP125A_HW6/CP125A_HW6/Model/JMSPhotoStore.h
+++ CP125A_HW6/CP125A_HW6/Model/JMSPhotoStore.h
@@ -14,6 +14,30 @@
 @property (strong, nonatomic)NSMutableArray *photoArray;
 
 #pragma mark - API
+/**
+ *  A singleton instance of the PhotoStore class
+ *
+ */
 + (instancetype)sharedStore;
+
+/**
+ *  Creates a new PhotoData object and adds it to the photoArray
+ *
+ *  @param image The photo image. Can't be nil
+ *  @param title The title of the image. Can be ni
+ *
+ *  @return The instance of the new photo
+ */
+- (JMSPhotoData *)addNewPictureToStoreWithImage:(UIImage *)image title:(NSString *)title;
+
+/**
+ *  Saves the current photoArray to disk.
+ */
+- (void)save;
+
+/**
+ *  Opens data saved to disk if it exists, and populates the photoArray.
+ */
+- (void)openSavedData;
 
 @end
--- CP125A_HW6/CP125A_HW6/Model/JMSPhotoStore.m
+++ CP125A_HW6/CP125A_HW6/Model/JMSPhotoStore.m
@@ -13,6 +13,15 @@ static NSString *const photoArrayKey = @"photoArray";
 
 @implementation JMSPhotoStore
 
+#pragma mark - Properties
+- (NSMutableArray *)photoArray
+{
+    if (!_photoArray) {
+        _photoArray = [NSMutableArray array];
+    }
+    return _photoArray;
+}
+
 #pragma mark - API
 + (instancetype)sharedStore
 {
@@ -20,8 +29,53 @@ static NSString *const photoArrayKey = @"photoArray";
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         _sharedInstance = [[JMSPhotoStore alloc] init];
+        [_sharedInstance openSavedData];
     });
     return _sharedInstance;
+}
+
+- (JMSPhotoData *)addNewPictureToStoreWithImage:(UIImage *)image title:(NSString *)title;
+{
+    NSAssert(image != nil, @"The image can't be nil");
+    
+    JMSPhotoData *photoData = [[JMSPhotoData alloc] init];
+    photoData.photo = image;
+    photoData.title = title;
+    
+    [[JMSPhotoStore sharedStore].photoArray addObject:photoData];
+    
+    [self save];
+    
+    return photoData;
+}
+
+- (void)save
+{
+    NSMutableArray *photoArray = [JMSPhotoStore sharedStore].photoArray;
+    NSMutableData *dataToWrite = [NSMutableData data];
+    
+    NSKeyedArchiver *archiver = [[NSKeyedArchiver alloc] initForWritingWithMutableData:dataToWrite];
+    archiver.requiresSecureCoding = YES;
+    [archiver encodeObject:photoArray forKey:photoArrayKey];
+    [archiver finishEncoding];
+    
+    NSString *saveLocation = [self savedFileLocation];
+    [dataToWrite writeToFile:saveLocation atomically:YES];
+}
+
+- (void)openSavedData
+{
+    NSData *savedData = [NSData dataWithContentsOfFile:[self savedFileLocation]];
+    
+    if (savedData) {
+        NSKeyedUnarchiver *unarchiver = [[NSKeyedUnarchiver alloc] initForReadingWithData:savedData];
+        unarchiver.requiresSecureCoding = YES;
+        NSSet *eligibleClasses = [NSSet setWithArray:@[[JMSPhotoData class], [NSMutableArray class]]];
+        NSMutableArray *savedPhotos = [unarchiver decodeObjectOfClasses:eligibleClasses forKey:photoArrayKey];
+        [unarchiver finishDecoding];
+        
+        self.photoArray = savedPhotos;
+    }
 }
 
 #pragma mark - NSSecureCoding
@@ -42,6 +96,17 @@ static NSString *const photoArrayKey = @"photoArray";
 + (BOOL)supportsSecureCoding
 {
     return YES;
+}
+
+#pragma mark - Private
+- (NSString *)savedFileLocation
+{
+    NSArray *directories = [[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask];
+    NSString *documentDirectory = [(NSURL *)[directories firstObject] path];
+    
+    NSString *photoArchive = [documentDirectory stringByAppendingPathComponent:@"photos"];
+    NSString *documentDirectoryString = [photoArchive stringByAppendingPathExtension:@"plist"];
+    return documentDirectoryString;
 }
 
 @end
