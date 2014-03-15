1447a2eab50e5fad41079873b4061c34a4210bda
Jared Sorge <jared@jsorge.net>
2014-03-15 12:27:13 -0700

    Added settings bundle and initial keys.

---
+++ CP125A_HW6/CP125A_HW6/JMSSettingsController.h
@@ -0,0 +1,16 @@
+//
+//  JMSSettingsController.h
+//  CP125A_HW6
+//
+//  Created by Jared Sorge on 3/15/14.
+//  Copyright (c) 2014 jsorge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString *const SETTING_ADD_TO_CAMERA_ROLL;
+extern NSString *const SETTING_ENABLE_EDIT_MODE;
+
+@interface JMSSettingsController : NSObject
+
+@end
---
+++ CP125A_HW6/CP125A_HW6/JMSSettingsController.m
@@ -0,0 +1,16 @@
+//
+//  JMSSettingsController.m
+//  CP125A_HW6
+//
+//  Created by Jared Sorge on 3/15/14.
+//  Copyright (c) 2014 jsorge. All rights reserved.
+//
+
+#import "JMSSettingsController.h"
+
+NSString *const SETTING_ADD_TO_CAMERA_ROLL = @"addToCameraRoll";
+NSString *const SETTING_ENABLE_EDIT_MODE = @"enableEditMode";
+
+@implementation JMSSettingsController
+
+@end
